547cd1bbac661e933d022c26e403f34e4f208f66
gustavo <gustavo.maia@b2b.com.pt>
2020-12-09 18:36:59 +0000

    AuthenticationCallback

--- app/src/main/java/com/artemissoftware/biometricauthentication/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/biometricauthentication/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.artemissoftware.biometricauthentication
 
+import android.content.Intent
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+
+    private var cancelationSignal: CancellationSignal? = null
+
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() =
+            @RequiresApi(Build.VERSION_CODES.P)
+            object : BiometricPrompt.AuthenticationCallback(){
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    notifyUser("Authentication error: $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+
+                    notifyUser("Authentication sucess")
+                    startActivity(Intent(this@MainActivity, SecretActivity::class.java))
+                }
+            }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+    private fun notifyUser(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
